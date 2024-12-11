f007c936f92b5a7ae8e23948cf7d105ab6d38544
letuanphat21 <ltphat240103@gmail.com>
2024-12-11 20:25:13 +0700

    Delete Doan directory
    ok

--- Doan/Doan/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Doan.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
